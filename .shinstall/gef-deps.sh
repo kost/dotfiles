71a6c4853bf8e047b7083e2292559c6eba3720fe
Vlatko Kosturjak <kost@linux.hr>
2017-11-05 05:51:51 +0100

    pips are also deps

--- .shinstall/gef-deps.sh
+++ .shinstall/gef-deps.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 apt-get install python3-pip
+# for Python2.x
+# $ pip2 install capstone unicorn keystone-engine ropper
+pip3 install capstone unicorn keystone-engine ropper retdec-python
 
--- .shinstall/gef-init.sh
+++ .shinstall/gef-init.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
 wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
-# for Python2.x
-# $ pip2 install capstone unicorn keystone-engine ropper
-pip3 install capstone unicorn keystone-engine ropper retdec-python
