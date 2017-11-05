e553ae2f1911557b28a081d8ae48e255df2ca09c
Vlatko Kosturjak <kost@linux.hr>
2017-11-05 07:54:15 +0100

    mac os x deps

--- .shinstall/gef-deps.sh
+++ .shinstall/gef-deps.sh
@@ -2,7 +2,9 @@
 
 apt-get install python3-pip
 apt-get install python-capstone libcapstone-dev
+# mac: brew install capstone keystone unicorn
 # for Python2.x
 # $ pip2 install capstone unicorn keystone-engine ropper
+
 pip3 install capstone unicorn keystone-engine ropper retdec-python
 
