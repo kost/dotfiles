efcc04c94b775f0eb0ce769985964875c0147bd3
Vlatko Kosturjak <kost@linux.hr>
2017-11-05 05:56:47 +0100

    capstone as dep

--- .shinstall/gef-deps.sh
+++ .shinstall/gef-deps.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 apt-get install python3-pip
+apt-get install python-capstone libcapstone-dev
 # for Python2.x
 # $ pip2 install capstone unicorn keystone-engine ropper
 pip3 install capstone unicorn keystone-engine ropper retdec-python
