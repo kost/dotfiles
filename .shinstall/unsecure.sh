4ea32b47551c132ffe4b718686e550dab572ca70
Vlatko Kosturjak <kost@linux.hr>
2025-09-06 08:33:48 +0200

    insecure installs

---
+++ .shinstall/unsecure.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+
+
