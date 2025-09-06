9ec22115f2e542af3a1663b7c47728c9234e482a
Vlatko Kosturjak <kost@linux.hr>
2025-09-06 08:36:06 +0200

    fix rvm and add pyenv

--- .shinstall/unsecure.sh
+++ .shinstall/unsecure.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+curl -fsSL https://pyenv.run | bash
+
 
 
 
