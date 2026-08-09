ff3f2925a39c8bb5163cc27664e42e17ad1be734
kost <vlatko.kosturjak@gmail.com>
2026-08-09 13:56:40 +0200

    Update git-clone.sh

--- .shinstall/git-clone.sh
+++ .shinstall/git-clone.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# git clone --bare https://github.com/kost/dotfiles $HOME/.cfg && cd $HOME/.cfg && git config --local status.showUntrackedFiles no && cd $HOME && git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 
 git clone --bare https://github.com/kost/dotfiles .cfg
 git config --local status.showUntrackedFiles no
